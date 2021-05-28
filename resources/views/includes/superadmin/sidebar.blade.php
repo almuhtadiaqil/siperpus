d982a897995b93e772b138fb39c224267050120d
unknown <Hishna.di@gmail.com>
2021-05-29 01:29:20 +0700

    pindah button logout

--- resources/views/includes/superadmin/sidebar.blade.php
+++ resources/views/includes/superadmin/sidebar.blade.php
@@ -4,7 +4,7 @@
     <a href="index3.html" class="brand-link">
         <img src="{{ asset('assets/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo"
             class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">Ini SuperAdmin</span>
+        <span class="brand-text font-weight-light">PT. HANIL JAYA STEEL</span>
     </a>
 
     <!-- Sidebar -->
@@ -79,12 +79,6 @@
                         class="nav-link {{ request()->is('superadmin/report') ? 'active' : '' }}">
                         <i class="nav-icon fas fa-print"></i>
                         <p>Report</p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="{{ url('logout') }}" class="nav-link">
-                        <i class="nav-icon fas fa-sign-out-alt"></i>
-                        <p>Logout</p>
                     </a>
                 </li>
             </ul>
